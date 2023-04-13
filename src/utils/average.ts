d7f83573c742f1a296bbd212e6ed1a78236a6407
Alexandru Bujor <bujoralexandru1996@gmail.com>
2023-04-13 13:25:04 +0300

    average update once every 5 seconds

--- src/utils/average.ts
+++ src/utils/average.ts
@@ -133,7 +133,7 @@ const main = async () => {
     );
     const log = averages(outputSubdirectories);
     logUpdate(log);
-  }, 1000);
+  }, 5000);
 
   process.on('exit', () => {
     clearInterval(interval);
